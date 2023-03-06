81904d1365d48cfd605045f3dbc3f8f42a953194
Suppasan23 <suppasan.benz@gmail.com>
2023-03-06 14:59:15 +0000

    10:00pm 6/3/2023

--- react-js/src/Component_Test1.js
+++ react-js/src/Component_Test1.js
@@ -1,4 +1,5 @@
-import './Component_Test1.css'
+import './Component_Test1.css';
+import { v4 as uuidv4 } from 'uuid';
 
 
 function Component_Test1(){
@@ -22,21 +23,26 @@ function Title(){
 
 //////////////////////////////////////////////////////////////////////////////////////
 function Transaction (){
+    const data = [
+        {title:"เงินเดือน", amount:"120000"},
+        {title:"ค่าที่พัก", amount:"30000"},
+        {title:"ค่าอาหาร", amount:"1000"},
+        {title:"ค่าเดินทาง", amount:"200"},
+        {title:"ค่าไฟฟ้า-ประปา", amount:"500"}
+    ]
     return(
         <ul className='transaction'>
-            <Item/>
-            <Item/>
-            <Item/>
+            <li className='li-bold'>รายการ<span>จำนวน</span></li>
+            <Item title={data[0].title} amount={data[0].amount} key={uuidv4()}/>
+            <Item title={data[1].title} amount={data[1].amount} key={uuidv4()}/>
+            <Item title={data[3].title} amount={data[3].amount} key={uuidv4()}/>
+            <Item title={data[4].title} amount={data[4].amount} key={uuidv4()}/>
         </ul>
     );
 }
 
-    function Item (){
-        const name = "เดินห้างซื้อของ"
-        const amount = 50000
-        const unit = "บาท"
-        return(
-            <li className='item'>{name}<span>{amount} {unit}</span></li>
-        );
-    }
-    
+function Item ({title, amount}){
+    return(
+        <li className='item'>{title}<span>{amount}</span></li>
+    );
+}
--- react-js/src/index.js
+++ react-js/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Component_Test1 from './Component_Test1';
 
-
-ReactDOM.render(<Component_Test1/>,document.getElementById('root1'));
-
+const root = ReactDOM.createRoot(document.getElementById("root1"));
+root.render(
+  <React.StrictMode>
+    <Component_Test1/>
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
